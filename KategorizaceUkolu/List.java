a090f0fea37839a03513842bcfe492dec9c88042
Dominik Slíva <sliva@SPSEJECNANET.CZ>
2021-05-13 10:39:59 +0200

    Kategorizace úkolů

---
+++ KategorizaceUkolu/List.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+public class List implements Ukoly {
+
+	
+	Scanner sc = new Scanner(System.in);
+	List<Ukol> list = new ArrayList<>();
+	
+	public void pridaniUkolu{
+		for()
+	}
+	
+	
+}
---
+++ KategorizaceUkolu/Pole.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Pole implements Ukoly {
+
+	String [] pole;
+	Scanner sc = new Scanner(System.in);
+	
+	public void pridaniUkolu{
+		for(int i = 0; i < pole.length; i++) {
+			pole[i] = ukol;
+			
+		}
+		
+	}
+	
+	public void najitUkol{
+		for(int i = 0; i < pole.length; i++) {
+			
+			
+		}
+		
+		
+	}
+}
